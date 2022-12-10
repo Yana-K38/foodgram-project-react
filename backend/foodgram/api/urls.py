330e950087b4f1c5478122025c5a547f471f1fff
Яна Курзюкова <kurzukova.yana@yandex.ru>
2022-12-10 16:59:55 +0500

    user

--- backend/foodgram/api/urls.py
+++ backend/foodgram/api/urls.py
@@ -13,6 +13,6 @@ router.register('users', CustomUserViewSet, basename='users')
 
 urlpatterns = (
     path('', include(router.urls)),
-    path('', include('djoser.urls')),
+  #  path('', include('djoser.urls')),
     path('auth/', include('djoser.urls.authtoken')),
 )
