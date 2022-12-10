464b3cb701ebcb1255c226c27cd389b0459c8615
Яна Курзюкова <kurzukova.yana@yandex.ru>
2022-12-10 17:09:43 +0500

    user

--- backend/foodgram/api/urls.py
+++ backend/foodgram/api/urls.py
@@ -9,10 +9,13 @@ router = routers.DefaultRouter()
 router.register('recipes', RecipeViewSet, basename='recipes')
 router.register('tags', TagViewSet, basename='tag')
 router.register('ingredients', IngredientViewSet, basename='Ingredient')
-router.register('users', CustomUserViewSet, basename='users')
+router.register(r'users/(?P<user_id>\d+)/subscribe)', CustomUserViewSet, basename='subscribe')
+router.register(r'users/subscriptions)', CustomUserViewSet, basename='subscriptions')
+
+
 
 urlpatterns = (
     path('', include(router.urls)),
-  #  path('', include('djoser.urls')),
+    path('', include('djoser.urls')),
     path('auth/', include('djoser.urls.authtoken')),
 )
