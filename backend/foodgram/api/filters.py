fd3458a1f64d01ee8ce2a9ea49938939c4295939
Яна Курзюкова <kurzukova.yana@yandex.ru>
2022-12-13 23:16:27 +0500

    favorite

--- backend/foodgram/api/filters.py
+++ backend/foodgram/api/filters.py
@@ -14,10 +14,11 @@ class RecipeFilter(rest_framework.FilterSet):
     tags = ModelMultipleChoiceFilter(
         field_name='tags__slug',
         to_field_name='slug',
-        queryset=Tag.objects.all()
+        queryset=Tag.objects.all(),
     )
     in_favorited = BooleanFilter(method='get_in_favorited')
     is_in_shopping_list = BooleanFilter(method='get_is_in_shopping_list')
+    author = rest_framework.CharFilter(lookup_expr='exact')
 
     class Meta:
         model = Recipe
@@ -29,10 +30,10 @@ class RecipeFilter(rest_framework.FilterSet):
 
     def get_in_favorited(self, queryset, name, value):
         if value:
-            return queryset.filter(
+            return Recipe.objects.filter(
                 favorite_in__user=self.request.user
             )
-        return queryset.exclude(favorite_in__user=self.request.user)
+        return Recipe.objects.exclude(favorite_in__user=self.request.user)
 
     def get_is_in_shopping_list(self, queryset, name, value):
         if value:
