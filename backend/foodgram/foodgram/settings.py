a65674ab77f7e3737d2e741264bdf4ce0e79fea7
Yana Kurzukova <kurzukova.yana@yandex.ru>
2023-01-04 13:51:31 +0500

    Update settings.py

--- backend/foodgram/foodgram/settings.py
+++ backend/foodgram/foodgram/settings.py
@@ -50,7 +50,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-CORS_ALLOWED_ORIGINS = ['http://localhost:3000',]
+CORS_ALLOWED_ORIGINS = ['http://localhost:3000', ]
 
 ROOT_URLCONF = 'foodgram.urls'
 
