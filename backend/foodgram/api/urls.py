7c149baa0481d89b575a72ab8d5ca114fa5074c8
Яна Курзюкова <kurzukova.yana@yandex.ru>
2023-01-06 15:01:59 +0500

    исправила urls

--- backend/foodgram/api/urls.py
+++ backend/foodgram/api/urls.py
@@ -7,13 +7,13 @@ from .views import (CustomUserViewSet, IngredientViewSet, RecipeViewSet,
 router = routers.DefaultRouter()
 
 router.register('recipes', RecipeViewSet, basename='recipes')
-router.register('tags', TagViewSet, basename='tag')
-router.register('ingredients', IngredientViewSet, basename='Ingredient')
+router.register('tags', TagViewSet, basename='tags')
+router.register('ingredients', IngredientViewSet, basename='Ingredients')
 router.register('users', CustomUserViewSet, basename='users')
 
 
-urlpatterns = (
+urlpatterns = [
     path('', include(router.urls)),
     path('', include('djoser.urls')),
     path('auth/', include('djoser.urls.authtoken')),
-)
+]
