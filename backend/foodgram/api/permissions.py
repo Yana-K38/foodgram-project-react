ca35f1bc1b4a84fa026de9458b552cadf14deed9
Яна Курзюкова <kurzukova.yana@yandex.ru>
2023-01-11 18:53:35 +0500

    permission

--- backend/foodgram/api/permissions.py
+++ backend/foodgram/api/permissions.py
@@ -13,8 +13,7 @@ class AdminOrAuthor(permissions.BasePermission):
     def has_object_permission(self, request, view, obj):
         return (
             request.method in permissions.SAFE_METHODS
-            or (request.user == obj.author)
-         #   or request.user.is_superuser
+            or request.user == obj.author
         )
 
 
